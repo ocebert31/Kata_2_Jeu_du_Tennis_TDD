f81cc95f244e0c253e97a579005e62fd1c718176
ocebert31 <153503666+ocebert31@users.noreply.github.com>
2024-07-02 15:56:25 +0200

    ajout de l avantage

--- index1.js
+++ index1.js
@@ -1,7 +1,7 @@
 // il y a X set (manche)
 // Nombre de points gagnés : 15 | 30 | 40 
 // Si 2 joueurs sont execo à 40, le prochain qui marque aura avantage, et ce dernier devra encore marquer pour gagner 1 jeu (point)
-// Si 2 joueurs sont à 15-40, selui à 40 gagne automatiquement
+// Si 2 joueurs sont à 15-40, celui à 40 gagne automatiquement
 // But : Calculer le score d'un jeu
 
 function assert(label, test) {
@@ -22,17 +22,20 @@ function calculateScore() {
     let playerAScore = 0;
     let playerBScore = 0;
     gameWins.forEach(win => {
-        if (win === 'player A' && playerAScore === 30) {
-            playerAScore += 10
+         if (win === 'player A' && playerAScore === 30) {
+            playerAScore += 10;
         } else if (win === 'player B' && playerBScore === 30) {
-            playerBScore += 10
-        } else if(win === 'player A') {
+            playerBScore += 10;
+        } else if(win === 'player A' && playerAScore < 40) {
             playerAScore += 15;
         } else if (win === 'player B') {
             playerBScore += 15;
+        } else if (win === 'player A' && playerAScore === 40 && playerBScore === 40) {
+            playerAScore = 'A'
         }
     });
     return `${playerAScore}-${playerBScore}`;
+
 }
 
 assert('0 points when the game starts', () => {
@@ -65,5 +68,19 @@ assert('40 points when the second player wins the game', () => {
     playerWins('player B');
     playerWins('player B');
     playerWins('player B');
+    console.log(calculateScore())
     return calculateScore() === '0-40';
-}); 
+});
+
+assert('xxx', () => {
+    gameWins = [];
+    playerWins('player A');
+    playerWins('player B');
+    playerWins('player A');
+    playerWins('player B');
+    playerWins('player A');
+    playerWins('player B');
+    playerWins('player A');
+    console.log(calculateScore())
+    return calculateScore() === 'A-40';
+});
