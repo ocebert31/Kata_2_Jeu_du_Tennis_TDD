4fabf7c71798ae0f9bedd5fbf05960f936111f60
ocebert31 <153503666+ocebert31@users.noreply.github.com>
2024-07-02 14:18:01 +0200

    2 tests quand l'un des joueurs a gagné la partie face à 0

--- index1.js
+++ index1.js
@@ -22,7 +22,11 @@ function calculateScore() {
     let playerAScore = 0;
     let playerBScore = 0;
     gameWins.forEach(win => {
-        if(win === 'player A') {
+        if (win === 'player A' && playerAScore === 30) {
+            playerAScore += 10
+        } else if (win === 'player B' && playerBScore === 30) {
+            playerBScore += 10
+        } else if(win === 'player A') {
             playerAScore += 15;
         } else if (win === 'player B') {
             playerBScore += 15;
@@ -32,16 +36,34 @@ function calculateScore() {
 }
 
 assert('0 points when the game starts', () => {
+    gameWins = [];
     return calculateScore() === '0-0';
 });
 
 assert('15 points when the player wins the first time', () => {
+    gameWins = [];
     playerWins('player A');
     return calculateScore() === '15-0';
 }); 
 
-assert('15 points when the second player wins the second time', () => {
+assert('15 points when the second player wins the first time', () => {
+    gameWins = [];
     playerWins('player B');
-    return calculateScore() === '15-15';
+    return calculateScore() === '0-15';
 }); 
 
+assert('40 points when the first player wins the game', () => {
+    gameWins = [];
+    playerWins('player A');
+    playerWins('player A');
+    playerWins('player A');
+    return calculateScore() === '40-0';
+}); 
+
+assert('40 points when the second player wins the game', () => {
+    gameWins = [];
+    playerWins('player B');
+    playerWins('player B');
+    playerWins('player B');
+    return calculateScore() === '0-40';
+}); 
